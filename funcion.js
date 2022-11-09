e05b4f56e64dc463bfd9ac41606614f88781f90c
YeimmiBuenaventura <yeimmipaola@gmail.com>
2022-11-09 17:41:57 -0500

    Codigo Documentado JS

--- funcion.js
+++ funcion.js
@@ -1,28 +1,27 @@
 const canvas = document.getElementById("canvas"); //llamamos al elemento canvas por su id
 var ctx = canvas.getContext("2d"); // Creamos una variable a la cual se le adjudique el contexto en formato 2d
 //Dibujando el tablero
-console.log("cxt", ctx);
+if(ctx === canvas.getContext("2d")){ //Se coloca un condicional en el cual se realiza un comparación del contexto en pro de ejecutar una serie de pasos que daran vida al tablero
+ctx.strokeStyle = "#272727";//Da el color a la linea del contexto
+ctx.strokeWidth = 0.001; //Da el grosor de la linea
 
-if(ctx === canvas.getContext("2d")){
-ctx.strokeStyle = "#272727";
-ctx.strokeWidth = 0.001;
-
-for (var i = 0; i <= canvas.width; i+=15){
-    ctx.beginPath();
-    ctx.moveTo(i, 0);
-    ctx.lineTo(i , ctx.canvas.height);
-    ctx.stroke();
+for (var i = 0; i <= canvas.width; i+=15){//Se implementa un ciclo for en el cual se inicializa la variable i en el pto 0, la condicion a ejecutar indica que se a realizar algo expuesto posteriormente hasta que se llegue al maximo del ancho; el aumento se realizara de 15 en 15
+    ctx.beginPath(); //Se indica que comience a dibujar
+    ctx.moveTo(i, 0);//El punto i = x en cuanto sistema de coordenadas, y el 0 = y
+    ctx.lineTo(i , ctx.canvas.height); //Se indica que dibuje una linea en cada una de las posiciones que tome i hasta llegar al limite del tablero
+    ctx.stroke();//Esta función trazara las lineas
 }
 
-for (var j = 0; j <= canvas.height; j+=15){
-    ctx.beginPath();
-    ctx.moveTo(0, j);
-    ctx.lineTo(ctx.canvas.width, j);  
-    ctx.stroke(); 
+for (var j = 0; j <= canvas.height; j+=15){//Se implementa un ciclo for en el cual se inicializa la variable i en el pto 0, la condicion a ejecutar indica que se a realizar algo expuesto posteriormente hasta que se llegue al maximo del alto; el aumento se realizara de 15 en 15
+    ctx.beginPath();//Se indica que comience a dibujar
+    ctx.moveTo(0, j);//El punto j = y en cuanto sistema de coordenadas, y el 0 = x
+    ctx.lineTo(ctx.canvas.width, j);  //Se indica que dibuje a lo ancho del contexto una linea en cada una de las posiciones que tome j hasta llegar al limite del tablero
+    ctx.stroke(); //Esta función trazara las lineas
 }      
 }
-var square = new Path2D('M 0 0 L 20 0 V 20 20 L 0 20 ');
-ctx.fill(square)
+//Trayendo figuras sobre el tablero: se indica una variable global a la cual se le brinda un nombre especifico de c/u de las figuras, indicando que es un nuevo Path2D el cual nos permite traer trazos realizados en svg a js como objeto
+var square = new Path2D('M 0 0 L 15 0 V 15 15 L 0 15 '); 
+ctx.fill(square) //Rellena la forma del contexto adjudicado
 var el = new Path2D('M 0 0 L 40 0 V 40 80 L80 80 V 80 120 L 0 120');
 ctx.fill();
 var barra = new Path2D('M 0 0 L 40 0 L 40 160 L 0 160'); 
@@ -35,6 +34,3 @@ var ese = new Path2D('M 40 0 L 120 0 L 120 40 L 80 40 L 80 80 L 0 80 L 0 40 L 40
 ctx.fill();
 var jota = new Path2D('M 40 0 L 80 0 L 80 120 L 0 120 L 0 80 L 40 80');
 
-
-
-//context.path([('b', 'm', 0, 0, 'l', 80, 0, 'l', 80, 80, 'l', 0, 80, 'c', 'f', 'red')]);
