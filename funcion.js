258568f00c91be34ecdc4ef2138eb2d70157a559
Juliana1497 <cristanchojuli@gmail.com>
2022-11-10 12:34:35 -0500

    agregando color figuras

--- funcion.js
+++ funcion.js
@@ -20,38 +20,44 @@ for (var j = 0; j <= canvas.height; j+=15){//Se implementa un ciclo for en el cu
 }      
 }
 //Trayendo figuras sobre el tablero: se indica una variable global a la cual se le brinda un nombre especifico de c/u de las figuras, indicando que es un nuevo Path2D el cual nos permite traer trazos realizados en svg a js como objeto
+ctx.fillStyle = 'blue';
 var square = new Path2D('M 0 0 L 15 0 V 15 15 L 0 15 '); 
-ctx.fill(square) //Rellena la forma del contexto adjudicado
+ctx.fill(square); //Rellena la forma del contexto adjudicado
+ctx.fillStyle = 'red';
 var el = new Path2D('M 0 0 L 15 0 V 15 30 L 30 30 V 30 45 L 0 45');
 ctx.fill();
+ctx.fillStyle = 'purple';
 var barra = new Path2D('M 0 0 L 15 0 L 15 60 L 0 60'); 
 ctx.fill();
+ctx.fillStyle = 'green';
 var zeta = new Path2D('M 0 0 L 30 0 V 30 15 L 45 15 V 45 30 L 15 30 V 15 15 L 0 15');
 ctx.fill();
+ctx.fillStyle = 'orange';
 var poli = new Path2D('M 0 0 L 45 0 L 45 15 L 30 15 V 30 30 L 15 30 L 15 15 L 0 15');
 ctx.fill();
+ctx.fillStyle = 'yellow';
 var ese = new Path2D('M 15 0 L 45 0 L 45 15 L 30 15 L 30 30 L 0 30 L 0 15 L 15 15');
 ctx.fill();
+ctx.fillStyle = 'white';
 var jota = new Path2D('M 15 0 L 30 0 L 30 45 L 0 45 L 0 30 L 15 30');
 ctx.fill();
 
-// const duracionCaida = 1.5
+const duracionCaida = 1.5
 
-// const crearCaida = (elem, steps, delay) => TweenLite.to(elem, duracionCaida, {
-//   y: '0%',
-//   ease: SteppedEase.config(steps),
-//   delay,
-// });
+const crearCaida = (elem, steps, delay) => TweenLite.to(elem, duracionCaida, {
+  y: '0%',
+  ease: SteppedEase.config(steps),
+  delay,
+});
 
 
-// // const squareDrop = createDrop(square, 10, 0.5)
-// const squareCaida = crearCaida(square, 10);
-// const eleCaida = crearCaida(el, 10);
-// const barraCaida = crearCaida(barra, 9);
-// const zetaCaida = crearCaida(zeta, 8);
-// const poliCaida = crearCaida(poli, 9);
-// const eseCaida = crearCaida(ese, 7);
-// const jotaCaida = crearCaida(jota, 7);
+const squareCaida = crearCaida(square, 10);
+const eleCaida = crearCaida(el, 10);
+const barraCaida = crearCaida(barra, 9);
+const zetaCaida = crearCaida(zeta, 8);
+const poliCaida = crearCaida(poli, 9);
+const eseCaida = crearCaida(ese, 7);
+const jotaCaida = crearCaida(jota, 7);
 
 
 
