56e606725480df2464a1b3ab95fcd3e8f06b3dc4
Juliana1497 <cristanchojuli@gmail.com>
2022-11-21 20:47:14 -0500

    ultimos arreglos

--- funcion.js
+++ funcion.js
@@ -1,46 +1,145 @@
 const canvas = document.getElementById("canvas"); //llamamos al elemento canvas por su id
-var ctx = canvas.getContext("2d"); // Creamos una variable a la cual se le adjudique el contexto en formato 2d
+const ctx = canvas.getContext("2d"); // Creamos una variable a la cual se le adjudique el contexto en formato 2d
+var x = canvas.width/2; //Variable que data la mitad del ancho 
+var y = canvas.height-150; // Variable que data la altura de caida de las piezas y
+var x_1 = (canvas.width/2) +10;//Variable que data la ubicación de la pieza en mitad del ancho de la pantalla más 10 px
+var y_1 = canvas.height-130;//Variable que data la altura de la caida de las piezas y_1
+var y_2 = canvas.height-140;//Variable que data la altura de la caida de las piezas y_2
+var dx = 0; //Variable que indica la cantidad de espacio que se movera la ficha hacia la X
+var dy = 1; //Varible que indica la "Velocidad con la que baja la pieza por el eje y"
+var up=38;//Variable adjudicada a la tecla que hace girar las fichas
+var left = 37; //Variable adjudicada a la tecla que corre hacia la derecha
+var right = 39; //Variable adjudicada a la tecla que corre hacia la izquierda
+var down = 40; //Variable adjudicada a la tecla que corre hacia abajo
+var pressing = [];
+
 //Dibujando el tablero
-if(ctx === canvas.getContext("2d")){ //Se coloca un condicional en el cual se realiza un comparación del contexto en pro de ejecutar una serie de pasos que daran vida al tablero
-ctx.strokeStyle = "#272727";//Da el color a la linea del contexto
-ctx.strokeWidth = 0.001; //Da el grosor de la linea
+function paint(){
+    if(ctx === canvas.getContext("2d")){ //Se coloca un condicional en el cual se realiza un comparación del contexto en pro de ejecutar una serie de pasos que daran vida al tablero
+        ctx.strokeStyle = "#000";//Da el color a la linea del contexto
+        ctx.lineWidth = 1; //Da el grosor de la linea
+        for (var i = 0; i <= canvas.width; i+=10){//Se implementa un ciclo for en el cual se inicializa la variable i en el pto 0, la condicion a ejecutar indica que se a realizar algo expuesto posteriormente hasta que se llegue al maximo del ancho; el aumento se realizara de 15 en 15
+            ctx.beginPath(); //Se indica que comience a dibujar
+            ctx.moveTo(i, 0);//El punto i = x en cuanto sistema de coordenadas, y el 0 = y
+            ctx.lineTo(i , ctx.canvas.height); //Se indica que dibuje una linea en cada una de las posiciones que tome i hasta llegar al limite del tablero
+            ctx.stroke();//Esta función trazara las lineas
+            ctx.closePath();
+        }
+        for (var j = 0; j <= canvas.height; j+=10){//Se implementa un ciclo for en el cual se inicializa la variable i en el pto 0, la condicion a ejecutar indica que se a realizar algo expuesto posteriormente hasta que se llegue al maximo del alto; el aumento se realizara de 15 en 15
+            ctx.beginPath();//Se indica que comience a dibujar
+            ctx.moveTo(0, j);//El punto j = y en cuanto sistema de coordenadas, y el 0 = x
+            ctx.lineTo(ctx.canvas.width, j);  //Se indica que dibuje a lo ancho del contexto una linea en cada una de las posiciones que tome j hasta llegar al limite del tablero
+            ctx.stroke(); //Esta función trazara las lineas
+            ctx.closePath();
+}}}
 
-for (var i = 0; i <= canvas.width; i+=15){//Se implementa un ciclo for en el cual se inicializa la variable i en el pto 0, la condicion a ejecutar indica que se a realizar algo expuesto posteriormente hasta que se llegue al maximo del ancho; el aumento se realizara de 15 en 15
-    ctx.beginPath(); //Se indica que comience a dibujar
-    ctx.moveTo(i, 0);//El punto i = x en cuanto sistema de coordenadas, y el 0 = y
-    ctx.lineTo(i , ctx.canvas.height); //Se indica que dibuje una linea en cada una de las posiciones que tome i hasta llegar al limite del tablero
-    ctx.stroke();//Esta función trazara las lineas
+
+// Función que crea la pieza j
+
+function jDraw(j){
+    ctx.beginPath();//Se inicializa el comienzo del dibujo
+    ctx.fillStyle='white';//Se indica el color que tendra el tetromino o pieza j
+    ctx.fillRect(x,y_1,10,10);//Se dibuja un rectangulo, teniendo en cuenta que los dos primeros valores indican la coordenada en donde se empieza a dibujar la figura, valores antes preestablecidos en las variables x, y_1
+    ctx.fillRect(x_1,y,10,30);//Se dibuja un rectangulo, teniendo en cuenta que los dos primeros valores indican la coordenada en donde se empieza a dibujar la figura, valores antes preestablecidos en las variables x_1, y
+    ctx.closePath();//Se cierra el dibujo de la figura
+    ctx.fill();
+    j=0;
 }
 
-for (var j = 0; j <= canvas.height; j+=15){//Se implementa un ciclo for en el cual se inicializa la variable i en el pto 0, la condicion a ejecutar indica que se a realizar algo expuesto posteriormente hasta que se llegue al maximo del alto; el aumento se realizara de 15 en 15
-    ctx.beginPath();//Se indica que comience a dibujar
-    ctx.moveTo(0, j);//El punto j = y en cuanto sistema de coordenadas, y el 0 = x
-    ctx.lineTo(ctx.canvas.width, j);  //Se indica que dibuje a lo ancho del contexto una linea en cada una de las posiciones que tome j hasta llegar al limite del tablero
-    ctx.stroke(); //Esta función trazara las lineas
-}      
+function squareDraw(o){
+    ctx.fillStyle='blue';//Se indica el color que tendra el tetromino o pieza cuadrado
+    ctx.beginPath();//Se inicializa el comienzo del dibujo
+    ctx.fillRect(x,y,20,20);//Se dibuja un rectangulo, teniendo en cuenta que los dos primeros valores indican la coordenada en donde se empieza a dibujar la figura, valores antes preestablecidos en las variables x, y
+    ctx.closePath();//Se cierra el dibujo de la figura
+    ctx.fill(); 
 }
-//Trayendo figuras sobre el tablero: se indica una variable global a la cual se le brinda un nombre especifico de c/u de las figuras, indicando que es un nuevo Path2D el cual nos permite traer trazos realizados en svg a js como objeto
-ctx.fillStyle = 'blue';
-var square = new Path2D('M 0 0 L 15 0 V 15 15 L 0 15 '); 
-ctx.fill(square); //Rellena la forma del contexto adjudicado
-ctx.fillStyle = 'red';
-var el = new Path2D('M 0 0 L 15 0 V 15 30 L 30 30 V 30 45 L 0 45');
-ctx.fill();
-ctx.fillStyle = 'purple';
-var barra = new Path2D('M 0 0 L 15 0 L 15 60 L 0 60'); 
-ctx.fill();
-ctx.fillStyle = 'green';
-var zeta = new Path2D('M 0 0 L 30 0 V 30 15 L 45 15 V 45 30 L 15 30 V 15 15 L 0 15');
-ctx.fill();
-ctx.fillStyle = 'orange';
-var poli = new Path2D('M 0 0 L 45 0 L 45 15 L 30 15 V 30 30 L 15 30 L 15 15 L 0 15');
-ctx.fill();
-ctx.fillStyle = 'yellow';
-var ese = new Path2D('M 15 0 L 45 0 L 45 15 L 30 15 L 30 30 L 0 30 L 0 15 L 15 15');
-ctx.fill();
-ctx.fillStyle = 'white';
-var jota = new Path2D('M 15 0 L 30 0 L 30 45 L 0 45 L 0 30 L 15 30');
-ctx.fill();
+ function lDraw(){
+    ctx.fillStyle='red';//Se indica el color que tendra el tetromino o pieza l
+    ctx.beginPath();//Se inicializa el comienzo del dibujo
+    ctx.fillRect(x,y,10,30);//Se dibuja un rectangulo, teniendo en cuenta que los dos primeros valores indican la coordenada en donde se empieza a dibujar la figura, valores antes preestablecidos en las variables x, y
+    ctx.fillRect(x_1,y_1,10,10);//Se dibuja un rectangulo, teniendo en cuenta que los dos primeros valores indican la coordenada en donde se empieza a dibujar la figura, valores antes preestablecidos en las variables x_1, y_1
+    ctx.closePath();//Se cierra el dibujo de la figura
+    ctx.fill();
+}
+
+function iDraw(){
+    ctx.fillStyle='purple';//Se indica el color que tendra el tetromino o pieza i
+    ctx.beginPath();//Se inicializa el comienzo del dibujo
+    ctx.fillRect(x,y,10,40);//Se dibuja un rectangulo, teniendo en cuenta que los dos primeros valores indican la coordenada en donde se empieza a dibujar la figura, valores antes preestablecidos en las variables x, y
+    ctx.closePath();//Se cierra el dibujo de la figura
+    ctx.fill();
+} 
+
+function zDraw(){
+    ctx.fillStyle='green';//Se indica el color que tendra el tetromino o pieza z
+    ctx.beginPath();//Se inicializa el comienzo del dibujo
+    ctx.fillRect(x,y,20,10);//Se dibuja un rectangulo, teniendo en cuenta que los dos primeros valores indican la coordenada en donde se empieza a dibujar la figura, valores antes preestablecidos en las variables x, y
+    ctx.fillRect(x_1,y_2,20,10);//Se dibuja un rectangulo, teniendo en cuenta que los dos primeros valores indican la coordenada en donde se empieza a dibujar la figura, valores antes preestablecidos en las variables x_1, y_1
+    ctx.closePath();//Se cierra el dibujo de la figura
+    ctx.fill();
+}
+ 
+function tDraw(){
+    ctx.fillStyle='orange';//Se indica el color que tendra el tetromino o pieza t
+    ctx.beginPath();//Se inicializa el comienzo del dibujo
+    ctx.fillRect(x,y,30,10);//Se dibuja un rectangulo, teniendo en cuenta que los dos primeros valores indican la coordenada en donde se empieza a dibujar la figura, valores antes preestablecidos en las variables x, y
+    ctx.fillRect(x_1,y_2,10,10);//Se dibuja un rectangulo, teniendo en cuenta que los dos primeros valores indican la coordenada en donde se empieza a dibujar la figura, valores antes preestablecidos en las variables x_1, y_2
+    ctx.closePath();//Se cierra el dibujo de la figura
+    ctx.fill();
+}
+
+function sDraw(){
+    ctx.fillStyle='yellow';//Se indica el color que tendra el tetromino o pieza s
+    ctx.beginPath();//Se inicializa el comienzo del dibujo
+    ctx.fillRect(x,y_2,20,10);//Se dibuja un rectangulo, teniendo en cuenta que los dos primeros valores indican la coordenada en donde se empieza a dibujar la figura, valores antes preestablecidos en las variables x, y_2
+    ctx.fillRect(x_1,y,20,10);//Se dibuja un rectangulo, teniendo en cuenta que los dos primeros valores indican la coordenada en donde se empieza a dibujar la figura, valores antes preestablecidos en las variables x_1, y
+    ctx.closePath();//Se cierra el dibujo de la figura
+    ctx.fill();
+}        
+
+
+//Función para brindar movimiento de caida a la pieza cuadrado
+function square()
+{
+    ctx.clearRect(0, 0, canvas.width, canvas.height); //Indicamos al canva que limpie los fotogramas "Recuerda que el movimiento generado esta dado por una serie de duplicados de la pieza que dejaran marcas de color"
+    jDraw(); //Llamamos la pieza a dibujar
+    //MOVIMIENTO CON TECLAS 
+    if(pressing[up])//Si se presiona la flecha para arriba
+    [ctx.translate(x+15, y+25),/*Se llama a la figura donde se crea una matriz para que su punto de giro sea en el centro de la figura
+    tendra punto de inicio de giro en la esquina inferior izquierda*/
+    ctx.rotate(90*Math.PI/360),//La figura rotara 45 grados multiplicados por el radio del circulo y dividido en 180 grados
+    ctx.translate(-x-15, -y-25),dx=1,dy=0]//Es necesario cerrar la matriz para que la figura gire para el opuesto de su centro 
+    if(pressing[right]) //Si se presiona la flecha para la derecha
+    [x+=10, x_1+=10]; //La figura se movera hacia la derecha con espacios de 10
+    if(pressing[down]) //Si se presiona la flecha para abajo
+    [y+=10,y_1+=10, y_2+=10];//La figura se movera hacia abajo con espacios de 10
+    if(pressing[left]) //Si se presiona la flecha a la izquierda
+    [x-=10, x_1-=10]; //La figura se movera hacia la izquierda con espacios de 10
+    if(x > canvas.width - 10 || x_1 > canvas.width - 10){ //Si x o x_1(eje horizontal) es mayor que el ancho del canvas -10(Se adjudica este valor para simular el espacio de la figura)
+      [x=canvas.width-20, x_1=canvas.width-10]; //Esto nos permite que el fotograma quede estatico al llegar al final del ancho del canvas
+    }
+    if(y > canvas.height - 10 || y_1 > canvas.height - 10 || y_2 > canvas.height - 10){//Si y o y_1 o y_2 (eje vertical) se pasa del alto del canvas respectivamente (Se adjudica este valor para simular el espacio de la figura) la figura se detendra
+    [y=canvas.height-30, y_1=canvas.height-10,y_2=canvas.height-10]; ////Esto nos permite que el fotograma quede estatico al llegar al final del alto del canvas
+    }
+    //Nos permite delimitar el canvas hacia los lados 
+    if(x<0 || x_1<0)
+        [x=0,x_1=10];  //x como coordenada 0
+    if(y<0 || y_1<0 || y_2<0)
+        [y=0,y_1=0,y_2=0];  //x como coordenada 0
+    x += dx; //Le indicamos el aumento dado para la X
+    y += dy; //Le indicamos el aumento dado para la Y
+    x_1 += dx;//Le indicamos el aumento dado para la X_1
+    y_1 += dy;//Le indicamos el aumento dado para la y_1
+    y_2 += dy;//Le indicamos el aumento dado para la y_2
+}
+document.addEventListener('keydown',function(evt){ // Estamos indicando que escuche el evento 'keydown'(Tecla Oprimida), ejecute (true) la funcion indicada por la el arreglo pressing de lo contrario no se ejecutara
+    pressing[evt.keyCode]=true;
+},false); 
+document.addEventListener('keyup',function(evt){ // Estamos indicando que escuche el evento 'keyup'(Tecla No Oprimida),  No ejecute (false) la funcion indicada por la el arreglo pressing
+    pressing[evt.keyCode]=false;
+},false);
+setInterval(square, 60);//setInterval es una función que nos permite llamar otra función y ejecutarla cada ciertos milisegundos, es directamente proporcional a nuestra variable dy
+
 
 
 
